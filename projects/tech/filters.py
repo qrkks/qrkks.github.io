4d36df5a28113bbf3d5f69d9920dd4a5d750cd8e
pp <34028312@qq.com>
2023-12-25 22:18:02 +0800

    po

--- projects/tech/filters.py
+++ projects/tech/filters.py
@@ -1,6 +1,19 @@
 from datetime import datetime
 
 def format_date_zh(value):
+    weekdays_en_to_zh = {
+        "Monday": "星期一",
+        "Tuesday": "星期二",
+        "Wednesday": "星期三",
+        "Thursday": "星期四",
+        "Friday": "星期五",
+        "Saturday": "星期六",
+        "Sunday": "星期日",
+    }
+    
     if isinstance(value, datetime):
-        return value.strftime('%Y年%m月%d日 %A %H:%M')
+        weekday_en = value.strftime('%A')
+        weekday_zh = weekdays_en_to_zh.get(weekday_en, weekday_en)
+        return value.strftime('%Y年%m月%d日 ') + weekday_zh + value.strftime(' %H:%M')
     return value
+
